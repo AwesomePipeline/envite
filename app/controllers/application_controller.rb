ac124690386de308d11d9374c252fbdfc8ab6704
Chunqi Zhu <chunqi93@gmail.com>
2015-06-10 00:34:28 +0800

    Rollback API helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
-  
-  rescue_from(ActionController::ParameterMissing) do |e|
-    error = {}
-    error[e.param] = ["parameter is missing"]
-    response = {errors: [error]}
-    respond_to do |format|
-      format.json {render json: response, status: :unprocessable_entity}
-    end
-  end
-  
-  # CRUD sequence conventions
-  # index
-  # show
-  # new
-  # edit
-  # create
-  # update
-  # destroy
 end
