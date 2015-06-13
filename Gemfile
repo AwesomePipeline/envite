b8eb0c624e3d6f6d2f2e76c70a7a65601a1282d2
Chunqi Zhu <chunqi93@gmail.com>
2015-06-13 18:05:53 +0800

    Removed broken fixtures, build should now pass

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+# Add simplecov gem
+gem 'simplecov', :require => false, :group => :test
+
 # Add coveralls.io gem
 gem 'coveralls', require: false
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,19 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+# Set up SimpleCov
+require 'simplecov'
+SimpleCov.start 'rails'
+
+require 'coveralls'
+Coveralls.wear! 'rails'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'coveralls'
-
-Coveralls.wear!('rails')
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include Devise::TestHelpers
 end
