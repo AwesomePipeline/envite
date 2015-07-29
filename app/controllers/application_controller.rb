4e881c54839ec3b4dd3a94b1570a723720f1a8bd
Chunqi Zhu <chunqi93@gmail.com>
2015-07-29 22:33:26 +0800

    Setup rails to handle CORS preflights

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # app/controllers/application_controller.rb
 
 class ApplicationController < ActionController::Base
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
   before_action :configure_permitted_parameters, :if => :devise_controller?
   
   # Returns a datetime object from specified key in params
@@ -47,6 +49,24 @@ class ApplicationController < ActionController::Base
       else
         logger.error "login failed"
       end
+    end
+  end
+  
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
     end
   end
 end
