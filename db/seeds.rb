7de4fc41ece6a109e5a48b7543347740b07cb862
Chunqi Zhu <chunqi93@gmail.com>
2015-08-04 17:56:50 +0800

    Fixed broken seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 admin = User.new
 admin.fullname = "Administrator"
-admin.user = "admin"
+admin.username = "admin"
 admin.admin = true
 admin.password = "12345678"
 admin.password_confirmation = "12345678"
@@ -16,7 +16,7 @@ admin.save!
 
 tester1 = User.new
 tester1.fullname = "Tester 1"
-tester1.user = "tester1"
+tester1.username = "tester1"
 tester1.admin = false
 tester1.password = "12345678"
 tester1.password_confirmation = "12345678"
@@ -24,7 +24,7 @@ tester1.save!
 
 tester2 = User.new
 tester2.fullname = "Tester 2"
-tester2.user = "tester2"
+tester2.username = "tester2"
 tester2.admin = false
 tester2.password = "12345678"
 tester2.password_confirmation = "12345678"
@@ -32,7 +32,7 @@ tester2.save!
 
 tester3 = User.new
 tester3.fullname = "Tester 3"
-tester3.user = "tester3"
+tester3.username = "tester3"
 tester3.admin = false
 tester3.password = "12345678"
 tester3.password_confirmation = "12345678"
